3453e7a67ab4c04c00842f08527ceb15110c3225
tomswift98 <liam.ginger.bennett@gmail.com>
2019-06-30 02:38:54 -0500

    add .gitignore, change cursor

--- config.h
+++ config.h
@@ -128,7 +128,7 @@ static unsigned int defaultrcs = 0;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 4;
 
 /*
  * Default columns and rows numbers
